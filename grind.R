26e3a1ea1b16d9b1a8d8addd30ab038b26a7188d
Dr Dennis Evangelista <devangel77b@gmail.com>
2024-12-27 12:26:27 -0500

    blah

--- grind.R
+++ grind.R
@@ -10,14 +10,26 @@ data <- mutate(
  )
 
 library(svglite)
+library(scales)
 fig2 <- ggplot(data)+geom_point(aes(x=mhat,y=ahat,color=source))+
-     ylim(0,0.3)+xlim(0,0.3)+
+     ylim(0,0.5)+xlim(0,0.5)+
      geom_abline(slope=1.0,intercept=0.0,color='blue')+
-     xlab('$\\frac{m_2}{m_1+m_2+m_c}$')+
-     ylab('$\\hat{a}$')+
+     xlab('$\\hat{m}$')+
+    ylab('$\\hat{a}$')+
+    scale_color_manual(values=hue_pal()(9),
+                         name=NULL,
+                         labels=c("\\citet{arenas-2024-testing}",
+                                  "\\citet{avalur-2024-verifying}",
+                                  "\\citet{canada-2024-experimental}",
+                                  "\\citet{govardhanen-2024-newtons}",
+                                  "\\citet{kedharnath-2024-examining}",
+                                  "\\citet{kishore-2024-relationship}",
+                                  "\\citet{krasnopolsky-2024-testing}",
+                                  "\\citet{perle-2024-experimental}",
+                                  "\\citet{yagnyeshwaran-2024-verifying}"))+
     theme_bw(base_size=8)+
-    guides(color=guide_legend(position="inside"))+
-    theme(legend.position.inside=c(0.1,0.7))
+    guides(color=guide_legend(position="right"))+
+    theme(aspect.ratio=1)
 svglite('fig3.svg',width=6,height=4,pointsize=8)
 print(fig2)
 dev.off()
