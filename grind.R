b7c082f5ef3d7f74000ab03da049c9cde01c9011
Dr Dennis Evangelista <devangel77b@gmail.com>
2024-12-27 11:03:28 -0500

    blah

--- grind.R
+++ grind.R
@@ -9,44 +9,18 @@ data <- mutate(
  ahat = ameas.ms2/9.81,
  )
 
-apredicted.ms2 <- function(m2,m1,mc){
-  9.81*m2/(m2+m1+mc)
-}
-
-fig <- ggplot(data)+geom_point(aes(x=m2.kg,y=ameas.ms2,color=source))+
-    ylim(0,4)+xlim(0,0.5)+
-    geom_function(fun=apredicted.ms2,args=list(m1=0.280,mc=0.5),color='blue')+
-    xlab('$m_2$ (\\unit{\\kilo\\gram})')+
-    ylab('$a$ (\\unit{\\meter\\per\\second\\squared})')+
-    theme_bw(base_size=8)
-
 library(svglite)
-svglite('fig3a.svg',width=3,height=2,pointsize=8)
-print(fig)
-dev.off()
-
-
 fig2 <- ggplot(data)+geom_point(aes(x=mhat,y=ahat,color=source))+
-     ylim(0,0.5)+xlim(0,0.5)+
-     geom_abline(slope=1.2,intercept=-0.02,color='blue')+
+     ylim(0,0.3)+xlim(0,0.3)+
+     geom_abline(slope=1.0,intercept=0.0,color='blue')+
      xlab('$\\frac{m_2}{m_1+m_2+m_c}$')+
-     ylab('$a$ (\\unit{\\meter\\per\\second\\squared})')+
-     theme_bw(base_size=8)
-svglite('fig3.svg',width=3,height=2,pointsize=8)
+     ylab('$\\hat{a}$')+
+    theme_bw(base_size=8)+
+    guides(color=guide_legend(position="inside"))+
+    theme(legend.position.inside=c(0.1,0.7))
+svglite('fig3.svg',width=6,height=4,pointsize=8)
 print(fig2)
 dev.off()
-
-library(xtable)
-results <- summarize(
-	mean.t = mean(t.s),
-	sd.t = sd(t.s),
-	mean.a = mean(ameas.ms2),
-	sd.a = sd(ameas.ms2),
-	group_by(data,m2.kg)
-	)
-print(xtable(results),include.rownames=FALSE,file='table1raw.tex')
-
-
 
 fit <- lm(ahat~mhat-1,data)
 summary(fit)
